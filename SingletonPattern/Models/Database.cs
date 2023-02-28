fb28e6c88fddf5d6e5820d2e8d16103ac10caedb
Jacek Kleczek <j.kleczek@me.com>
2023-02-28 18:49:19 +0100

    first commit

---
+++ SingletonPattern/Models/Database.cs
@@ -0,0 +1,19 @@
+namespace SingletonPattern.Models
+{
+    public class Database
+    {
+        private static Database? _databaseInstance;
+        public static Database GetDatabaseInstance()
+        {
+            if (_databaseInstance is null)
+            {
+                _databaseInstance = new();
+            }
+            return _databaseInstance;
+        }
+        private Database()
+        {
+
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using SingletonPattern.Models;
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Database database1 = Database.GetDatabaseInstance();
+            Database database2 = Database.GetDatabaseInstance();
+
+            if (database1 == database2)
+            {
+                Console.WriteLine("Database1 is equal to database2.");
+            }
+        }
+    }
+}
